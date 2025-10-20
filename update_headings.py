b232b8767e6de0f0463a1c5b70fb3271b5ccd2bd
Martin Green <martin.speleo@gmail.com>
2025-10-20 20:01:36 +0100

    Not sure if this is still a usefull script or not ...

---
+++ update_headings.py
@@ -0,0 +1,16 @@
+import sys, os, csv, re
+from spreadsheet_functions import *
+from datetime import datetime, date
+from wirral_config import *
+
+print(sys.argv)
+
+if len(sys.argv) <= 1:
+    print("Please area code")
+    sys.exit()
+    
+areas = sys.argv[1]
+
+for area in areas:
+    reorder(area)
+
